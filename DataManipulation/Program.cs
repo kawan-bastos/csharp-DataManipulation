4c1960fdd97581dd75e164df4c0c14a822ff60b6
kawan-bastos <kawanbastos021@gmail.com>
2026-08-06 21:19:31 -0300

    escreve primeiras linha do primeiro exercicio

---
+++ DataManipulation/Program.cs
@@ -0,0 +1,28 @@
+﻿var diasDaSemana = new DiasDaSemana();
+
+var carrinho = new List<Produto>
+{
+    new Produto { Nome = "Oleo", Preco = 10.0 },
+    new Produto { Nome = "Leite", Preco = 5.50 },
+    new Produto { Nome = "Ketchup", Preco = 7.49 }
+};
+
+PercorrendoComFor();
+void PercorrendoComFor()
+{
+    for (int i = 0; i < carrinho.Count; i++)
+    {
+        Console.WriteLine($"Produto: {carrinho[i].Nome}, Preço: {carrinho[i].Preco}");
+    }
+}
+class Produto
+{
+    public string Nome { get; set; }
+    public double Preco { get; set; }
+};
+
+class DiasDaSemana
+{
+    public string[] dias = { "Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado" };
+}
+
