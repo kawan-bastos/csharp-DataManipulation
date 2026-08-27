5b2f8fde7f05a07ac54b17fec1b5d031c58eb932
kawan-bastos <kawanbastos021@gmail.com>
2026-08-27 06:42:24 -0300

    Opera com projecoes

--- 04-LINQ/Program.cs
+++ 04-LINQ/Program.cs
@@ -4,7 +4,7 @@
     Implemente as funções abaixo:
     //     [x] Leia-o como uma coleção de músicas
     //     [x] Filtre a coleção por artista (por ex. Coldplay, Metallica, AC/DC)
-    //     [ ] Filtre a coleção por gênero (por ex. rock)
+    //     [x] Filtre a coleção por gênero (por ex. rock)
     //     [x] Filtre a coleção por duração (por ex. maiores que 5 minutos)
     //     [x] Ordene a coleção por artista
     //     [x] Ordene a coleção por artista e em seguida por músicas com duração crescente
@@ -20,17 +20,41 @@ using System.Runtime.CompilerServices;
 using var arquivo = new FileStream("C:\\Users\\kawan\\source\\repos\\DataManipulation\\03-AbstraindoAFonteDeDados\\musicas.csv", FileMode.Open, FileAccess.Read);
 using var stream = new StreamReader(arquivo);
 
-var musicas = ObterArquivo(stream);
-//ExibirMusicas(musicas);
 
-var musicasColdplay =
+var generos = ObterArquivo(stream)
+                .SelectMany(m => m.Genero) // projecao ou transformacao 
+                .Distinct() // filtragem
+                .OrderBy(g => g);
+
+foreach (var genero in generos)
+{
+    Console.WriteLine(genero);
+}
+
+void OperacoesComProjecoes(StreamReader stream)
+{
+    var artistas = ObterArquivo(stream)
+                .Select(m => m.Artista) // projecao ou transformacao 
+                .Distinct() // filtragem5
+                .OrderBy(a => a);
+
+    foreach (var artista in artistas)
+    {
+        Console.WriteLine(artista);
+    }
+}
+void OperacoesDeFiltroEOrdenacao(StreamReader stream)
+{
+    var musicasColdplay =
     ObterArquivo(stream)
     .Where(m => m.Artista == "Coldplay")
-    .OrderBy(m => m.Titulo)  
-    //.OrderBy(musica => musica.Artista)
+    .OrderBy(m => m.Titulo)   // ThenBy (m => m.Duracao)                             
+                              //.OrderBy(musica => musica.Artista)   isso permite que a gente ordene por mais de um critério, primeiro por artista e depois por duração.
+                              // Skip(5) pula os 5 primeiros elementos da coleção
     .Take(5);
     //.OrderBy(m => m.Duracao); // mais conciso e legivel! isso se chama expressao lambda com LINQ (Language Integrated Query) - uma forma de consultar coleções de dados em C#. 
-ExibirMusicas(musicasColdplay);
+    ExibirMusicas(musicasColdplay);
+}
 
 void ExibirMusicas(IEnumerable<Musica> musicas)
 {
@@ -53,7 +77,8 @@ IEnumerable<Musica> ObterArquivo(StreamReader stream)
         {
             Titulo = separador[0],
             Artista = separador[1],
-            Duracao = Convert.ToInt32(separador[2])
+            Duracao = Convert.ToInt32(separador[2]),
+            Genero = separador[3].Split(',').Select(g => g.Trim())
         };
         yield return musica;
         linha = stream.ReadLine();
@@ -78,6 +103,7 @@ class Musica
     public string Titulo { get; set; }
     public string Artista { get; set; }
     public int Duracao { get; set; }
+    public IEnumerable<string> Genero { get; set; }
 }
 
 
